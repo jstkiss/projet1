4cbd6a28670922e0065df4a71a7b44803116f22f
JSTKode <mehdi.cesa@outlook.fr>
2023-01-06 11:18:51 +0100

    update

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -1,41 +1,41 @@
-"use client";
+'use client';
 
-import { motion } from "framer-motion";
-import styles from "../styles";
-import { fadeIn } from "../utils/motion";
+import { motion } from 'framer-motion';
+import styles from '../styles';
+import { fadeIn } from '../utils/motion';
 
 const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
   <motion.div
     variants={fadeIn('right', 'spring', index * 0.5, 0.75)}
     className={`relative ${
-      active === id ? "lg:flex-[3.5] flex-[10]" : "lg:flex-[0.5] flex-[2]"
+      active === id ? 'lg:flex-[3.5] flex-[10]' : 'lg:flex-[0.5] flex-[2]'
     } flex items-center justify-center min-w-[170px] h-[700px] transition-[flex] duration-[0.7s] ease-out-flex cursor-pointer`}
     onClick={() => handleClick(id)}
   >
     <img
       src={imgUrl}
       alt={title}
-      className="absolute w-full h-full object-cover rounded-[24px]"
+      className='absolute w-full h-full object-cover rounded-[24px]'
     />
     {active !== id ? (
-      <h3 className="font-semibold sm:text-[26px] text-[18px] text-white absolute z-0 lg:bottom-20 lg:rotate-[-90deg] lg:origin-[O,0]">
+      <h3 className='font-semibold sm:text-[26px] text-[18px] text-white absolute z-0 lg:bottom-20 lg:rotate-[-90deg] lg:origin-[O,0]'>
         {title}
       </h3>
     ) : (
-      <div className="absolute bottom-0 p-8 justify-start w-full flex-col bg-[rgba(0,0,0,0.5)] rounded-b-3xl">
+      <div className='absolute bottom-0 p-8 justify-start w-full flex-col bg-[rgba(0,0,0,0.5)] rounded-b-3xl'>
         <div
           className={`${styles.flexCenter} w-14 h-14 rounded-3xl glassmorphism mb-4`}
         >
           <img
-            src="headset.svg"
-            alt="head"
-            className="w-1/2 h-1/2 object-contain"
+            src='headset.svg'
+            alt='head'
+            className='w-1/2 h-1/2 object-contain'
           />
         </div>
-        <p className="font-normal text-base leading-5 text-white uppercase">
+        <p className='font-normal text-base leading-5 text-white uppercase'>
           Enter The Multivers
         </p>
-        <h2 className="mt-6 font-semibold sm:text-text-2xl text-2xl text-white">
+        <h2 className='mt-6 font-semibold sm:text-text-2xl text-2xl text-white'>
           {title}
         </h2>
       </div>
--- components/NewFeatures.jsx
+++ components/NewFeatures.jsx
@@ -1,13 +1,13 @@
-import styles from "../styles";
+import styles from '../styles';
 
 
 const NewFeatures = ({ imgUrl, title, subtitle}) => (
-  <div className="flex-1 flex flex-col sm:max-w-64 min-w-52">
+  <div className='flex-1 flex flex-col sm:max-w-64 min-w-52'>
     <div className={`${styles.flexCenter} w-16 h-16 rounded-3xl bg-[#323f5d]`}>
-      <img src={imgUrl} alt="icon" className="w-1/2 h-1/2 object-contain" />
+      <img src={imgUrl} alt='icon' className='w-1/2 h-1/2 object-contain' />
     </div>
-      <h1 className="mt-6 font-bold text-base leading-7 text-white">Title {title}</h1>
-      <p className="flex-1 leading-8 mt-4 font-normal text-lg text-[#b0b0b0] ">{subtitle}</p>
+      <h1 className='mt-6 font-bold text-base leading-7 text-white'>Title {title}</h1>
+      <p className='flex-1 leading-8 mt-4 font-normal text-lg text-[#b0b0b0] '>{subtitle}</p>
   </div>
 );
 
