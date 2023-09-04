777d7e0a0d930255e8756404eb866ed18da72257
JSTKode <mehdi.cesa@outlook.fr>
2023-09-04 04:42:39 +0200

    update

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-import { Navbar } from "../components";
+import { Navbar } from '../components';
 import {
   About,
   Explore,
@@ -7,7 +7,7 @@ import {
   Hero,
   Insights,
   WhatsNew,
-} from "../sections";
+} from '../sections';
 
 const Page = () => (
   <div className="bg-primary-black overflow-hidden">
