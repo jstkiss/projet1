b2c1ed634fc0f53b018674170ba18690b10add14
JSTKode <mehdi.cesa@outlook.fr>
2023-09-04 04:37:27 +0200

    update

--- app/page.js
+++ app/page.js
@@ -16,17 +16,17 @@ const Page = () => (
     <div className="relative">
       <About />
       <div className="gradient-03 z-0" />
-        <Explore />
+      <Explore />
     </div>
     <div className="relative">
       <GetStarted />
-      <div className="gradient-04 z-0" />  
-        <WhatsNew />
+      <div className="gradient-04 z-0" />
+      <WhatsNew />
     </div>
     <div className="relative">
       <Insights />
       <div className="gradient-05 z-0" />
-        <Feedback />
+      <Feedback />
     </div>
   </div>
 );
